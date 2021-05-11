243eab3f60e7012fa57d42cbbb36f316b7fef388
Rio-18 <riyayashmore@gmail.com>
2021-05-11 18:25:10 +0530

    step sixth done

--- arth.sh
+++ arth.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -x
 
+declare -A dict
 echo "welcome to arithmatical computation and sorting"
 
 read -p "enter first value : " a
@@ -7,13 +8,17 @@ read -p "enter second value : " b
 read -p "enter third value : " c
 
 sum=$(echo $a $b $c | awk '{print $1+$2*$3}')
-echo $sum
+dict['sum']=$sum 
 
 sum2=$(echo $a $b $c | awk '{print $1+$3/2}')
-echo $sum2
+dict['sum2']=$sum2
 
 sum3=$(echo $a $b $c | awk '{print $1*$2+$3}')
-echo $sum3
+dict['sum3']=$sum3
 
 sum4=$(echo $a $b $c | awk '{print $1*$2+$3}')
-echo $sum4
+dict['sum4']=$sum4
+
+echo ${dict[@]}
+
+
